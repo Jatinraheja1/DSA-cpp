fad19a27753436d412ce776446338dedf342db46
Jatinraheja1 <jatinraheja02235@gmail.com>
2026-09-08 10:20:59 +0530

    pattern 13 solved

---
+++ pattern/P13.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+void print1(int n)
+{
+    class Solution
+    {
+    public:
+        void pattern13(int n)
+        {
+            int a = 1;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 1; j <= i + 1; j++)
+                {
+                    cout << a << " ";
+                    a++;
+                }
+                cout << endl;
+            }
+        }
+    };
+}
+int main()
+{
+    int n;
+    cout << "enter no. of rows :";
+    cin >> n;
+    print1(n);
+    return 0;
+}
